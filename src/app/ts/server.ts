ad5030b348ecc77adc335b0f135d8fb4d890356a
ushibutatory <ushibutatory@gmail.com>
2021-03-19 01:44:29 +0900

    comment: revise

--- src/app/ts/server.ts
+++ src/app/ts/server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { readFile } from "fs";
 
-// run application.
 const PORT = process.env.PORT || 8080;
 
 const app: express.Express = express();
@@ -10,11 +9,9 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req: express.Request, res: express.Response) => {
-  // load file
   readFile("data/birthdays.ical", (error, data) => {
     if (error) throw error;
 
-    // send
     res.setHeader("Content-Type", "text/calendar; charset=UTF-8");
     res.status(200).send(data);
   });
