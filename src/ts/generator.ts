8bea28df099258de3c7dba2df0c2462be649dd9c
ushibutatory <ushibutatory@gmail.com>
2023-09-16 22:03:25 +0900

    move files

--- src/ts/generator.ts
+++ src/ts/generator.ts
@@ -10,7 +10,7 @@ export class Generator {
   /**
    * 元になるデータが格納されているディレクトリ
    */
-  private readonly _resourceDirectory: string = "../../data";
+  private readonly _resourceDirectory: string = "../master";
 
   /**
    * 配信用データを格納するディレクトリ
